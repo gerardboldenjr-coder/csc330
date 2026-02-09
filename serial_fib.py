171ac74b465a194cc24099cccadfbed01cf20006
gerardboldenjr-coder <gerard.boldenjr@svu.edu>
2026-02-08 21:56:16 -0800

    Update serial_fib.py

--- serial_fib.py
+++ serial_fib.py
@@ -1,1 +1,23 @@
+import time
 
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+def main():
+    nums = [35, 36, 37, 38, 39]
+
+    start = time.time()
+
+    for n in nums:
+        answer = fib(n)
+        print("fib(" + str(n) + ") =", answer)
+
+    end = time.time()
+    total = end - start
+
+    print("\nTotal time:", total, "seconds")
+
+if __name__ == "__main__":
+    main()
