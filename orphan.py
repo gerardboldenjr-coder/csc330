8a3a03d28b5d02d48093a30f9223fa2cb386be15
gerardboldenjr-coder <gerard.boldenjr@svu.edu>
2026-02-10 19:12:56 -0800

    Update orphan.py

--- orphan.py
+++ orphan.py
@@ -1,1 +1,13 @@
+import os
+import time
 
+pid = os.fork()
+
+if pid == 0:
+    print(f"Child ({os.getpid()}): Sleeping for 30 seconds...")
+    print(f"Child's original parent PID: {os.getppid()}")
+    time.sleep(30)
+    print(f"Child ({os.getpid()}) finished")
+else:
+    print(f"Parent ({os.getpid()}) started")
+    print(f"Parent ({os.getpid()}) finished")
